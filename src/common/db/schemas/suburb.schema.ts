5b6fc9c080222b8be2e298f9cfde1fa8474e70ca
anandkr06 <tech.anandkr06@gmail.com>
2026-01-26 22:52:13 +0530

    subub to map boundarites

--- src/common/db/schemas/suburb.schema.ts
+++ src/common/db/schemas/suburb.schema.ts
@@ -21,7 +21,30 @@ export class Suburb extends Document {
 
   @Prop({ default: true })
   public isActive!: boolean;
+
+  @Prop({
+    type: {
+      type: String,
+      enum: ['Polygon', 'MultiPolygon'],
+      required: false
+    },
+    coordinates: {
+      type: [],
+      required: false
+    }
+  })
+  geometry?: {
+    type: 'Polygon' | 'MultiPolygon';
+    coordinates: any;
+  };
+  
 }
 
 export type SuburbDocument = Suburb & Document & { _id: Types.ObjectId };
 export const SuburbSchema = SchemaFactory.createForClass(Suburb);
+SuburbSchema.index({ geometry: '2dsphere' });
+
+
+// schema.index({ geometry: '2dsphere' });
+// schema.index({ locality: 1, state: 1 });
+
