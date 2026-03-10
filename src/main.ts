ce9a39008e646e87f179cb7b891133099c56000b
anandkr06 <tech.anandkr06@gmail.com>
2026-03-10 12:14:02 +0530

    webhook debug

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,20 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import * as bodyParser from 'body-parser';
-// const cookieParser = require('cookie-parser');
-
-// async function bootstrap() {
-
-//   const app = await NestFactory.create(AppModule, {
-//     rawBody: true,
-//   });
-
-//   app.use(cookieParser());
-
-//   // // Stripe webhook raw body
-//   // app.use(
-//   //   '/api/webhooks/stripe',
-//   //   bodyParser.raw({ type: 'application/json' }),
-//   // );
-
-//   app.setGlobalPrefix('api');
-//   app.enableCors({ origin: '*' });
-
-//   await app.listen(3001);
-
-//   console.log('🚀 Server running on http://localhost:3001');
-// }
-
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
 
-  const app = await NestFactory.create(AppModule, {
-    rawBody: true,
-  });
-
-  app.use(cookieParser());
+  app.use(
+    '/api/webhooks/stripe',
+    bodyParser.raw({ type: 'application/json' }),
+  );
 
   app.setGlobalPrefix('api');
   app.enableCors({ origin: '*' });
 
-  await app.listen(3001);
-
-  console.log('🚀 Server running on http://localhost:3001');
+  await app.listen(3000);
 }
-
 bootstrap();
+
