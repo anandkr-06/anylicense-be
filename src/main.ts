dacaba9ef1bcf7353c70714ab6567a7069764205
anandkr06 <tech.anandkr06@gmail.com>
2026-03-10 12:40:43 +0530

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
-const cookieParser = require('cookie-parser');
-
+const cookieParser = require("cookie-parser"); // ✅ CORRECT
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false, // 🔥 REQUIRED
+  });
 
+  // 🔥 Stripe webhook MUST be raw
   app.use(
     '/webhooks/stripe',
     bodyParser.raw({ type: 'application/json' }),
   );
+  app.use(cookieParser());   // 👈 REQUIRED
+  // ✅ JSON for rest of app
+  app.use(bodyParser.json());
 
   app.setGlobalPrefix('api');
   app.enableCors({ origin: '*' });
 
   await app.listen(3001);
+  console.log('🚀 Server running on http://localhost:3001');
 }
-bootstrap();
 
+bootstrap();
